dad3a37702e21c00a60a11907776becc3168ca81
Witold Urbas <aiur18@student.bth.se>
2019-12-06 22:57:44 +0000

    v1.0.6

--- config/apikeys.php
+++ config/apikeys.php
@@ -5,11 +5,12 @@
  /**
  ** ipstack1 5743a124d24d6c68f6dc20dccab02eac
  ** ipstack2 9d318e8c489a613ebfe73d94a35c3cfa
+** ipstack3 deba45f2c00989d0855209e849866d20
  ** datsky1 c938d7a8b2a93d07ac994129372ad516
  ** darksky2 dc529b66f2d366a2edaf3225641c69fe
-  *
+  ** darksky3 df5348b78d6de9140ebfb0db5c819b31
   */
  return [
-     "ipstack"       => "9d318e8c489a613ebfe73d94a35c3cfa",
-     "darksky"       => "dc529b66f2d366a2edaf3225641c69fe"
+     "ipstack"       => "deba45f2c00989d0855209e849866d20",
+     "darksky"       => "df5348b78d6de9140ebfb0db5c819b31"
  ];
