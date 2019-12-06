24bd81decfe76bb552cd86df43c717fcb6bf5711
Witold Urbas <aiur18@student.bth.se>
2019-12-06 16:26:38 +0000

    fourth commit

--- config/apikeys.php
+++ config/apikeys.php
@@ -3,9 +3,13 @@
  * api
  */
  /**
+ ** ipstack1 5743a124d24d6c68f6dc20dccab02eac
+ ** ipstack2 9d318e8c489a613ebfe73d94a35c3cfa
+ ** datsky1 c938d7a8b2a93d07ac994129372ad516
+ ** darksky2 dc529b66f2d366a2edaf3225641c69fe
   *
   */
  return [
-     "ipstack"       => "5743a124d24d6c68f6dc20dccab02eac",
-     "darksky"       => "c938d7a8b2a93d07ac994129372ad516"
+     "ipstack"       => "9d318e8c489a613ebfe73d94a35c3cfa",
+     "darksky"       => "dc529b66f2d366a2edaf3225641c69fe"
  ];
